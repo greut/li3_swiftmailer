a700ea8dcdc66cd2ba716c039a1728691e0a0f72
Yoan Blanc <yoan@dosimple.ch>
2012-02-15 11:58:45 +0100

    Adding `domain` configuration support for SMTP transport.

--- extensions/adapter/mailer/transport/Smtp.php
+++ extensions/adapter/mailer/transport/Smtp.php
@@ -3,10 +3,25 @@
 namespace li3_swiftmailer\extensions\adapter\mailer\transport;
 
 /**
- * Transport relying on an SMTP server
+ * Transport relying on a SMTP server.
+ *
+ * Options:
+ *
+ * - `'host'` SMTP server hostname
+ * - `'port'` SMTP server port
+ * - `'encryption'` what kind of encryption is used _none_, `tls` or `ssl`
+ * - `'username'` if login is required the username
+ * - `'password'` if login is required the password
+ * - `'domain'` which domain is advertised while doing the EHLO phase
+ *    (leave empty if unsure)
+ *
+ * @see li3_swiftmailer\mailer\Transport
  */
 class Smtp extends \li3_swiftmailer\mailer\Transport
 {
+	/**
+	 * The keys that are automagically configured.
+	 */
 	protected $config = array('host', 'port', 'encryption', 'username', 'password');
 
 	protected function _init() {
@@ -14,6 +29,19 @@ class Smtp extends \li3_swiftmailer\mailer\Transport
 			'transport' => '\\Swift_SmtpTransport',
 		);
 		return parent::_init();
+	}
+
+	/**
+	 * Do a special trick to set the localDomain using the domain key.
+	 *
+	 * @see li3_swiftmailer\mailer\Transport::_config()
+	 */
+	protected function _config() {
+		parent::_config();
+		if (isset($this->_config['domain'])) {
+			$transport = $this->get_transport();
+			$transport->setLocalDomain($this->_config['domain']);
+		}
 	}
 }
 
