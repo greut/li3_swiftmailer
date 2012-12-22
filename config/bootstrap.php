3030f923d0b2fbcfedf92263a92b9bb14bd3afb2
djordje <djordje.bigjoe@gmail.com>
2012-12-22 01:14:09 +0100

    Added 2nd path for loading 'swiftmailer' fetched to composer vendor dir

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -2,8 +2,26 @@
 
 use \lithium\core\Libraries;
 
+/**
+ * Composer SWIFTMAILER path:
+ * `/../vendor/swiftmailer/swiftmailer/lib/classes`
+ *
+ * Git clone SWIFTMAILER path:
+ * `/swiftmailer/lib/classes`
+ */
 if (!defined('SWIFTMAILER_LIBRARY_PATH')) {
+	/**
+	 * Git clone SWIFTMAILER path
+	 * Comment if use composer path
+	 */
 	$path = '/swiftmailer/lib/classes';
+
+	/**
+	 * Composer SWIFTMAILER path
+	 * Uncomment if use composer path
+	 */
+	// $path = '/../vendor/swiftmailer/swiftmailer/lib/classes';
+
 	$path = file_exists(LITHIUM_LIBRARY_PATH . $path) ?
 		LITHIUM_LIBRARY_PATH . $path :
 		LITHIUM_APP_PATH . "/libraries/{$path}";
