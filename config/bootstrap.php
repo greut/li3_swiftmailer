100eeba0120a2b5afe9db3bbbcf9ff55c0173bab
Yoan Blanc <yoan@dosimple.ch>
2013-01-09 19:25:57 +0100

    Tested with SwiftMailer 4.3.0.

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -3,10 +3,10 @@
 use \lithium\core\Libraries;
 
 if (!defined('SWIFTMAILER_LIBRARY_PATH')) {
-	$path = '/swiftmailer/lib/classes';
+	$path = '/swiftmailer/lib';
 	$path = file_exists(LITHIUM_LIBRARY_PATH . $path) ?
 		LITHIUM_LIBRARY_PATH . $path :
-		LITHIUM_APP_PATH . "/libraries/{$path}";
+		LITHIUM_APP_PATH . '/libraries/' . $path;
 	define('SWIFTMAILER_LIBRARY_PATH', $path);
 }
 
@@ -15,7 +15,7 @@ $library = Libraries::get($name);
 
 if (empty($library)) {
 	Libraries::add($name, array(
-		'bootstrap' => '/../swift_required.php',
+		'bootstrap' => 'swift_required.php',
 		'path' => SWIFTMAILER_LIBRARY_PATH,
 		'prefix' => 'Swift_'
 	));
